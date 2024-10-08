3657c48a284479d10271f5580d7fd717fc48eb34
nguyen_le_khuong@gmail.com <nguyen_le_khuong@optimizer.co.jp>
2024-10-08 17:30:55 +0700

    Cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,9 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
             
         ]);
 
-        $middleware->use([
+        $middleware->append([
             HandleCors::class,
         ]);
+
 
         $middleware->trustProxies(at: '*');
         $middleware->statefulApi();
